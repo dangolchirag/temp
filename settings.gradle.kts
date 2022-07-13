d27474eb1e97c57a6899cec56f164335616b94fc
dangolchirag <dangol.chirag@gmail.com>
2022-07-13 15:48:42 +0545

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,9 @@
+rootProject.name = "AndroidAppTemplate"
+include(
+    ":app",
+    ":domain",
+    ":data:core",
+    ":data:features:webapi:trivia",
+    ":data:features:webapi:unsplash",
+    ":data:features:auth"
+)
